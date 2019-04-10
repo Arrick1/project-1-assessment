37084d6d72dc295e5ec2f8ea98c075c24eab49aa
Arrick <Grapes01@gmail.com>
2019-04-10 15:37:19 -0700

    minus button functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 // console.log("working")
+
+// add button 
 document.getElementById("add").addEventListener("click", function(){
     document.getElementById("display").innerText = "working";
+});
+
+document.getElementById("minus").addEventListener("click", function(){
+    document.getElementById("display").innerText = "also";
 });
