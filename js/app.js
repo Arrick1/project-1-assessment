10de7cd373a784398d28a78d028fd03a56b65a05
Arrick <Grapes01@gmail.com>
2019-04-10 17:41:41 -0700

    finished

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 // console.log("working")
 
+
 // add button 
+let x = document.getElementById("input");
+let y = document.getElementById("display").innerText
+
+// const sum = (total, x) =>{
+//   return total + x;  
+// };
+
 document.getElementById("add").addEventListener("click", function(){
-    document.getElementById("display").innerText = "working";
+    
+    let currentValue = y
+    let number = parseInt(currentValue)
+    let inputNumber = parseInt(x.value)
+    document.getElementById("display").innerText = number + inputNumber
 });
 
+
+
+// minus button 
 document.getElementById("minus").addEventListener("click", function(){
-    document.getElementById("display").innerText = "also";
+    let currentValue = y;
+    let newNumber = parseInt(currentValue);
+    let newInputNumber = parseInt(x.value)
+    document.getElementById('display').innerText =newNumber - newInputNumber;
 });
